d63d1764f6f77d91345d03ec5954cd05b395aea7
Valeriy Horoshev <155026160+Valeriy9horoshev@users.noreply.github.com>
2023-12-28 00:46:11 +0200

    Update alert report.go

--- alert report.go
+++ alert report.go
@@ -10,3 +10,17 @@ func NewSpamMigitationAnteDecorator(pylonsmodulekeeper PylonsKeeper) AnteSpamMig
 
 func (ad AnteSpamMigitationDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (sdk.Context, error) {
 	if (ctx.IsCheckTx() || ctx.IsReCheckTx()) && !simulate {}
+
+
+	func TestOtherTransactionIsValid(t *testing.T) {
+	// Set MaxTxsInBlock  = 2
+	numberTxsinBlocks := 2
+	config := network.ConfigWithMaxTxsInBlock(uint64(numberTxsinBlocks))
+	net := network.New(t, config)
+			args = append(args, common...)
+	res, _ := clitestutil.ExecTestCLICmd(ctx, bank.NewSendTxCmd(), args)
+	return res
+
+			require.NoError(t, ctx.Codec.UnmarshalJSON(validRes.Bytes(), &resp))
+	require.Equal(t, successCode, resp.Code)
+}
